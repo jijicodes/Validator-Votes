0b192d197c36a98215e3c6eaf4a7d22c57f580d0
jijicodes <sjieun11@gmail.com>
2022-05-26 16:12:19 -0600

    added tally result with grommet meter bar

---
+++ src/components/ProposalTallyVote/ProposalTallyVote.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Box, Meter, Text, Stack } from "grommet";
+import { StopFill } from "grommet-icons";
+
+export const ProposalTallyVote = ({ tallyValue }) => {
+  return (
+    <Box gap="large">
+      <Box alignSelf="center">
+        <Stack anchor="bottom">
+          <Meter
+            round
+            thickness="large"
+            type="bar"
+            values={[
+              {
+                value: Number(tallyValue.yes),
+                color: "purple",
+              },
+
+              {
+                value: Number(tallyValue.no),
+                color: "red",
+              },
+              {
+                value: Number(tallyValue.abstain),
+                color: "gray",
+              },
+              {
+                value: Number(tallyValue.no_with_veto),
+                color: "blue",
+              },
+            ]}
+          />
+        </Stack>
+      </Box>
+      <Box direction="row" align="center" justify="around">
+        <Box></Box>
+        <Box></Box>
+        <Box direction="row" gap="xsmall">
+          <StopFill color="purple" />
+          <Text> Yes</Text>
+        </Box>
+        <Box direction="row" gap="xsmall">
+          <StopFill color="red" />
+          <Text>No</Text>
+        </Box>
+        <Box direction="row" gap="xsmall">
+          <StopFill color="gray" />
+          <Text>Abstain</Text>
+        </Box>
+        <Box direction="row" gap="xsmall">
+          <StopFill color="blue" />
+          <Text>No with Veto</Text>
+        </Box>
+        <Box></Box>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+};
